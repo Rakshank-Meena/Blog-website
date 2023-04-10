94fd4346f46005b6630017ec626feca3c6f9c2a6
Rakshank <rakshank.meena@gmail.com>
2023-04-11 03:08:28 +0530

    dynamic import changes

---
+++ Components/Layout.js
@@ -0,0 +1,16 @@
+import dynamic from "next/dynamic"
+
+const Navbar =dynamic(()=>import("./Navbar")) 
+
+const Layout = ({ children }) => {
+    return (
+        <>
+        <div className="content">
+            <Navbar/>
+            {children}
+        </div>
+        </>
+    )
+}
+
+export default Layout
