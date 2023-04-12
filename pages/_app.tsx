d14f3e586bf93b1961ada3e0a4d653d26a1632b9
Rakshank <rakshank.meena@gmail.com>
2023-04-12 07:03:25 +0530

    lazy loading components with ssr false

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import dynamic from 'next/dynamic'
-const Layout = dynamic(() => import("../components/Layout"), { ssr: false })
+// const Layout = dynamic(() => import("../components/Layout"))
+import Layout from "../components/Layout"
 export default function App({ Component, pageProps }: AppProps) {
 
   return (
